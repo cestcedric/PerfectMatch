ee1d87aba063b06708fda9d0df68d6e62ff3a4e4
Cédric Stark <cedric.stark@tum.de>
2021-02-06 20:14:23 +0100

    Add number of matches parameter + formatting

--- args.py
+++ args.py
@@ -1,9 +1,11 @@
 import argparse
 from   datetime import datetime
+import math
 
 ap = argparse.ArgumentParser()
 ap.add_argument('--id', type = str, default = None, help = 'ID for solver run')
-ap.add_argument('--limit', type = int, default = 4*10e5, help = 'Limit number of tries')
+ap.add_argument('--matches', type = int, default = 10, help = 'Matches in a game (default: 10)')
+ap.add_argument('--limit', type = int, default = None, help = 'Limit number of tries (default: matches!)')
 ap.add_argument('--output', type = str, default = 'output', help = 'Output directory for graphs (default: output)')
 ap.add_argument('--rounds', type = int, default = 2, help = 'Seasons of PerfectMatch played (default: 1)')
 ap.add_argument('--seed', type = int, default = 69420, help = 'Seed for match generation (default: 69420)')
@@ -14,4 +16,12 @@ my_args = ap.parse_args()
 
 if my_args.id == None:
     now = datetime.now()
-    my_args.id = str(now.year) + str(now.month) + str(now.day) + str(now.hour) + str(now.minute) + str(now.second)
+    my_args.id = str(now.year).zfill(4) \
+        + str(now.month).zfill(2) \
+        + str(now.day).zfill(2) \
+        + str(now.hour).zfill(2) \
+        + str(now.minute).zfill(2) \
+        + str(now.second).zfill(2)
+
+if my_args.limit == None:
+    my_args.limit = math.factorial(my_args.matches)
