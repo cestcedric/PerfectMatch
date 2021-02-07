46df1a8a201c6f2380dceb9f9ce6216b6aac9f1b
Cedric Stark <cedric.stark@tum.de>
2021-02-07 19:25:17 +0100

    Update logger to directly see changes in file

--- util/logger.py
+++ util/logger.py
@@ -1,13 +1,16 @@
 import sys
 
 class Logger(object):
+
     def __init__(self, outputpath):
         self.terminal = sys.stdout
-        self.log = open(outputpath, 'a')
+        self.log = outputpath
 
     def write(self, message):
         self.terminal.write(message)
-        self.log.write(message)  
+        log = open(self.log, 'a')
+        log.write(message)
+        log.close()
 
     def flush(self):
         pass    
