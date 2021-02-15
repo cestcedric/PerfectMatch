0da64d441bb1bd5430a973836b4909cf9299de92
Cedric Stark <cedric.stark@tum.de>
2021-02-15 23:07:09 +0100

    Implement Bumblesort

--- solvers/__init__.py
+++ solvers/__init__.py
@@ -1,11 +1,13 @@
 from .Solver import *
 from .Bumblesort import *
+from .Bumblesort_v2 import *
 from .Random import *
 from .TestRotate import *
 from .Tryhard import *
 
 __all__ = (
         'Bumblesort',
+        'Bumblesort_v2',
         'Random',
         'TestRotate',
         'Tryhard'
